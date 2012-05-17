ad77bec798b2db8123d65e1063aadce0bfcbec28
Elizabeth Leddy <eleddy@eastofeaton.com>
2012-05-16 21:09:31 -0700

    Account profile page created. Refs #14

--- src/noisebridge.pyclass/noisebridge/pyclass/urls.py
+++ src/noisebridge.pyclass/noisebridge/pyclass/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
-from pyclass.profiles import views
+from pyclass.profiles import views as profile_views
+import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,10 +10,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'pyclass.views.index'),
-    url(r'^search/$', views.search_interests),
-    url(r'^addinterest/$', views.add_interests),
-    url(r'^addinterest/interest_submitted/$', views.interest_submitted),
-    url(r'^display_avatar$', views.display_avatar),
+    url(r'^search/$', profile_views.search_interests),
+    url(r'^users/(?P<user_name>\w+)/$', views.users),
+    url(r'^addinterest/$', profile_views.add_interests),
+    url(r'^addinterest/interest_submitted/$', profile_views.interest_submitted),
+    url(r'^display_avatar$', profile_views.display_avatar),
 
     # Sets up default URLs for django-registration
     #url(r'^accounts/', include('registration.backends.default.urls')),
@@ -25,4 +27,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/noisebridge.pyclass/noisebridge/pyclass/views.py
+++ src/noisebridge.pyclass/noisebridge/pyclass/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def users(request, user_name):
+    return render(request, "profile.html", {"user_name":user_name})
