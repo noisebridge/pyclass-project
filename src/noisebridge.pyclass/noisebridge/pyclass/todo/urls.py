3e380411ddd7d945e4b14d81653b37230c10677c
Mark Smith <belgand@gmail.com>
2012-05-30 09:20:28 -0700

    Add page listing all ToDo items in db. Close #32.

--- src/noisebridge.pyclass/noisebridge/pyclass/todo/urls.py
+++ src/noisebridge.pyclass/noisebridge/pyclass/todo/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from pyclass.todo.models import ToDoItem
 
 urlpatterns = patterns('pyclass.todo.views',
     #url(r'^$', 'pyclass.views.index'),
     url(r'^details/(?P<pk>\d+)/$', DetailView.as_view(model=ToDoItem), name="todoitem_details"),
+    url(r'^list/$', ListView.as_view(model=ToDoItem, paginate_by=20)),
 )
